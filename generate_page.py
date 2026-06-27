4c0fd156084bdcbe874ee1286cad2243b9d599f1
chelmidispring <michel.lammens@medispring.be>
2026-06-27 02:53:27 +0200

    Faders: ajoute Master + 2 faders hazer (Debit fumee=fog, Ventilo=fan) -> 4 master faders

--- generate_page.py
+++ generate_page.py
@@ -665,16 +665,25 @@ LYRE_IDS = [1736278739, 1736279250, 1736279257, 1736279265]
 PAR_ID = 1748027678
 def flist(ids, ch):
     return "".join("%d,%s|" % (i, ch) for i in ids)
-# MODE MINIMAL (pour le moment) : UN SEUL master fader = "Dimmer" general (dimmer de TOUTES les machines).
-# Les autres faders sont retires. Pour les Krypton, le dimmer n'agit que si la LAMPE est ON et le shutter
-# ouvert -> presser INIT d'abord (il strike les lampes et ouvre les shutters), ensuite ce fader gere l'intensite.
-all_dim = (flist([x for x in KR_IDS], "dimmer") + flist([x[0] for x in MB], "dimmer")
+# Master faders (sliders) actuels :
+#  fader0 = Dimmer  (intensite de toutes les machines)
+#  fader1 = Master  (grand master toutes machines, comme l'ancien fader 'Master' -> coupe/niveau general)
+#  fader2 = Debit fumee (canal hazer 'fog')      |  fader3 = Ventilo (canal hazer 'fan')
+# NB Krypton : le dimmer n'agit que lampe ON + shutter ouvert -> presser INIT d'abord.
+# NB hazer : un master fader sur 1 canal pilote ce canal (meme principe que l'ancien fader 'PAR Ambiance').
+all_dim = (flist(list(KR_IDS), "dimmer") + flist([x[0] for x in MB], "dimmer")
            + flist(LYRE_IDS, "dimmer") + flist([PAR_ID], "dimmer")
            + flist([BLINDER[0][0]], "dimmer") + flist([LASER[0][0]], "dimmer"))
+hazer_fog = flist([HAZER[0][0]], "fog")
+hazer_fan = flist([HAZER[0][0]], "fan")
 mf = ("[master_faders]\n"
-      "type_fader0 = 0\ncaption_fader0 = Dimmer\nv8_master_fader0 = %s\n") % all_dim
+      "type_fader0 = 0\ncaption_fader0 = Dimmer\nv8_master_fader0 = %s\n"
+      "type_fader1 = 0\ncaption_fader1 = Master\nv8_master_fader1 = %s\n"
+      "type_fader2 = 0\ncaption_fader2 = Debit fumee\nv8_master_fader2 = %s\n"
+      "type_fader3 = 0\ncaption_fader3 = Ventilo\nv8_master_fader3 = %s\n"
+     ) % (all_dim, all_dim, hazer_fog, hazer_fan)
 content = re.sub(r'master_faders = \d+\n', '', content)                   # retire toute ancienne ligne
-content = content.replace("[live]\n", "[live]\nmaster_faders = 1\n", 1)  # 1 seul fader (Dimmer general) pour le moment
+content = content.replace("[live]\n", "[live]\nmaster_faders = 4\n", 1)  # Dimmer + Master + Debit + Ventilo
 # Bindings faders physiques APC40 -> master faders. STRATEGIE PRESERVATION :
 #  - si des bindings 'faderN_midi' existent deja (appris dans l'UI via MIDI learn), on NE LES TOUCHE PAS
 #    -> le mapping appris survit aux regenerations ;
